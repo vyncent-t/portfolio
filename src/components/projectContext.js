074d732980a88d885162b42891a1d7854be15911
Vincent T <vyncode@gmail.com>
2021-09-02 17:44:42 -0400

    adding tools text to cards

--- src/components/projectContext.js
+++ src/components/projectContext.js
@@ -17,7 +17,7 @@ let proA = {
     desc: "Travlr is a social media platform in which users express their experiences at venues or establishments in the form of recommendation posts, creating value by ways of exposure for establishments and reliable entertainment/information for the user base. Travlr is meant to be used primarily at a local level by tourists to better inform and make judgments based on engagement of the users native to their destination/city.",
     deploy: "https://travlr-proj.herokuapp.com/",
     repo: "https://github.com/vyncent-t/travlr-project",
-    tech: "Javascript, React.js, Bootstrap, MongoDB, Mongoose, GraphQL, HTML, CSS, Git, GitHub, Apollo, Insomnia"
+    tech: "Tools: Javascript, React.js, Bootstrap, MongoDB, Mongoose, GraphQL, HTML, CSS, Git, GitHub, Apollo, Insomnia"
 }
 
 let proB = {
@@ -35,7 +35,7 @@ let proC = {
     desc: "Atmos is a multi-media consumption app that allows the user to select both a preferred genre of music to be listened to through spotify access while viewing a playlist of curated visual atmospheric videos to be played simultaneously based off of keywords searched by the user. This provides a unique relaxation experience to the user only by streamlining both separate sources of audio and visuals in a single platform instance.",
     deploy: "https://vyncent-t.github.io/atmosphere-project/",
     repo: "https://github.com/vyncent-t/atmosphere-project",
-    tech: "Javascript, jQuery, HTML, CSS, Spotify API, Youtube API"
+    tech: "Tools: Javascript, jQuery, HTML, CSS, Spotify API, Youtube API"
 }
 
 projectArr.push(proA)
