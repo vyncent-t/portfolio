dc739f5a8d59a9dcc27bee5a7c95c92758b783ad
Vincent T <vyncode@gmail.com>
2021-09-02 17:29:44 -0400

    updating bio

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
                     <div>
                         <h1>About Me</h1>
                         <p>
-                            Hey! I'm Vincent, a full-stack developer located in South Florida. I been working on this portfolio to serve as a showcase for all of my current and future projects. I specialize in both front-end and back-end development with my foundation being built using the MERN stack.
+                            Hey! I'm Vincent, a passionate, self-motivated aspiring Software Engineer located in South Florida. I am always eager for opportunities to further enrich current knowledge and master the latest skills and tools needed for cutting edge web development. An analytical thinker with a knack for problem solving, eager to explore new and creative approaches to any challenges. MERN Stack developer with strong proficiency in front-end works of HTML, CSS, and JavaScript with an emphasis on the React JavaScript framework while using Node js and a wide variety of NPM tools/packages. Completed a Certificate in Full Stack Web Development at the University of Miami. Excited to apply honed skills in creating user-friendly interfaces while onboard your team.
                         </p>
                     </div>
                 </div>
