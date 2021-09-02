a5db9ea2f4e2632e5d3dd93669a1031c04d6b25c
Vincent T <vyncode@gmail.com>
2021-09-02 18:50:41 -0400

    updating image links

--- src/components/projectContext.js
+++ src/components/projectContext.js
@@ -13,7 +13,7 @@ const projectArr = []
 
 let proA = {
     title: "Travlr",
-    image: "https://raw.githubusercontent.com/vyncent-t/portfolio-project/main/my-portfolio/src/images/atmossc.jpg",
+    image: "https://github.com/vyncent-t/portfolio-project/blob/main/src/images/travlapp.jpg?raw=true",
     desc: "Travlr is a social media platform in which users express their experiences at venues or establishments in the form of recommendation posts, creating value by ways of exposure for establishments and reliable entertainment/information for the user base. Travlr is meant to be used primarily at a local level by tourists to better inform and make judgments based on engagement of the users native to their destination/city.",
     deploy: "https://travlr-proj.herokuapp.com/",
     repo: "https://github.com/vyncent-t/travlr-project",
@@ -22,7 +22,7 @@ let proA = {
 
 let proB = {
     title: "Likeminded",
-    image: "imgplace",
+    image: "https://github.com/vyncent-t/portfolio-project/blob/main/src/images/likemindsc.jpg?raw=true",
     desc: "Likeminded is a multi-user organizational app that allows users to create, propose, and manage activities within a group of other individual users on the platform. This app serves as a tool to better link users to each other through friend groups labeled as cliques which contain dynamic events created by the users where they propose plans to be decided on as a collective.",
     deploy: "https://likeminded-project.herokuapp.com/",
     repo: "https://github.com/vyncent-t/likeminded",
@@ -31,7 +31,7 @@ let proB = {
 
 let proC = {
     title: "atmos",
-    image: "https://raw.githubusercontent.com/vyncent-t/portfolio-project/main/my-portfolio/src/images/atmossc.jpg",
+    image: "https://github.com/vyncent-t/portfolio-project/blob/main/src/images/atmossc.jpg?raw=true",
     desc: "Atmos is a multi-media consumption app that allows the user to select both a preferred genre of music to be listened to through spotify access while viewing a playlist of curated visual atmospheric videos to be played simultaneously based off of keywords searched by the user. This provides a unique relaxation experience to the user only by streamlining both separate sources of audio and visuals in a single platform instance.",
     deploy: "https://vyncent-t.github.io/atmosphere-project/",
     repo: "https://github.com/vyncent-t/atmosphere-project",
