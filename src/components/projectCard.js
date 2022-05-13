b1dbacc0e08a6b19744e87dfe0858497da6e0b2f
Vincent T <vyncode@gmail.com>
2022-05-13 17:50:58 -0400

    css changes

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -9,12 +9,12 @@ function ProjectCard() {
     console.log(projectArr)
 
     return (
-        <div className="container d-flex flex-wrap justify-content-around boxshadow">
+        <div className="container d-flex flex-wrap justify-content-around">
             <div className='row'>
                 {projectArr.map((project) => (
                     <div className="col-lg-4 my-3">
                         <div className="card shadow-sm text-dark">
-                            <div className="card-body">
+                            <div className="card-body boxshadow">
                                 <h3 className="card-title">{project.title}</h3>
                                 <img src={project.image} className="card-img-top mb-3 boxshadow" alt="..." />
                                 <p>
@@ -25,16 +25,16 @@ function ProjectCard() {
                                         <a href={project.demo} target="_blank" rel="noopener noreferrer">Video Demo</a>
                                     </p>
                                 </div>))}
-                                <small className="text-muted">{project.tech}</small>
+                                <small>{project.tech}</small>
                                 <div className="d-flex justify-content-center align-items-center">
                                     <div className="btn-group">
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                                        <button type="button" className="btn btn-sm ">
                                             <i className="bi bi-laptop"></i>
                                             <a href={project.deploy}
                                             >View project</a
                                             >
                                         </button>
-                                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                                        <button type="button" className="btn btn-sm ">
                                             <i className="bi bi-github"></i>
                                             <a href={project.repo}
                                             >Github</a
