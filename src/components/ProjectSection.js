f5b8fbae88619c8cf6af348f996f1de9ac0f7cd3
Vincent T <vyncode@gmail.com>
2022-05-16 18:09:31 -0400

    edit

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -12,7 +12,7 @@ function ProjectSection() {
                 <div id="projects" class="m-4 p-4 text-white rounded-3 bgcolor boxshadow">
                     <h2>My projects</h2>
                     <p>
-                        I pride myself on my ability to take an idea from an inspired thought,to the whiteboard, and finally from my screen and on to yours!
+                        I pride myself on my ability to take an idea from an inspired thought, to the whiteboard, and finally from my screen and on to yours!
                         Being able to create new and engaging projects for myself and others and building them from the ground up is what keeps me motivated.
                         I believe within each project is an opportunity to experiment and learn new methods and technologies to further sharpen my skills not only as a developer but as a creative.
                     </p>
